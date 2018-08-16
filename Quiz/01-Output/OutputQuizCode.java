fc5012a31e57b36e6d533a9594eaa56aaaf8db5c
Matt Weeks <m_weeks@hotmail.co.uk>
2018-08-16 13:11:52 +0100

    Content.

--- Quiz/01-Output/OutputQuizCode.java
+++ Quiz/01-Output/OutputQuizCode.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class OutputQuizCode {
     public static void main(String[] args) {
         qu01();
@@ -195,5 +198,268 @@ public class OutputQuizCode {
             i++;
         }
         System.out.println("End");
+    }
+
+    private static void qu31(int size) {
+        String[] words = new String[size];
+        System.out.println(words.length);
+        System.out.println(Arrays.toString(words));
+    }
+
+    private static void qu32(int size) {
+        int[] numbers = new int[size];
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void qu33(int number) {
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        numbers[1] = 13;
+        numbers[2] = 73;
+        numbers[3] = 37;
+        numbers[4] = number;
+        System.out.println(numbers.length);
+        System.out.println(Arrays.toString(numbers));
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(i + ": " + numbers[i]);
+        }
+    }
+
+    private static void qu34(int newNumber1, int newNumber2) {
+        int[] numbers = new int[5];
+        numbers[0] = 3;
+        numbers[1] = 13;
+        numbers[2] = 73;
+        numbers[3] = 37;
+        numbers[4] = 21;
+        System.out.println(Arrays.toString(numbers));
+        numbers[2] = newNumber1;
+        numbers[3] = newNumber2;
+        System.out.println(Arrays.toString(numbers));
+    }
+
+    private static void qu35(String name) {
+        String[] dogName = new String[5];
+        dogName[0] = "Bella";
+        dogName[1] = "Lucy";
+        dogName[2] = name;
+        dogName[3] = "Amber";
+        dogName[4] = "Lola";
+        System.out.println(Arrays.toString(dogName));
+    }
+
+    private static void qu36(int number) {
+        Object[] things = new Object[5];
+        things[0] = "dog";
+        things[1] = number;
+        things[2] = "Storm";
+        things[3] = 4.21f;
+        things[4] = 'c';
+        System.out.println(Arrays.toString(things));
+    }
+
+    private static void qu37() {
+        ArrayList<String> todo = new ArrayList<>();
+        todo.add("Step 1: Learn to code");
+        todo.add("Step 2: Create an app");
+        todo.add("Step 3: ...");
+        todo.add("Step 4: Profit");
+        System.out.println(todo.toString());
+    }
+
+    private static void qu38() {
+        ArrayList<String> todo = new ArrayList<>();
+        todo.add("Step 1: Learn to code");
+        todo.add("Step 2: Create an app");
+        todo.add("Step 3: ...");
+        todo.add("Step 4: Profit");
+        for (String item : todo) {
+            System.out.println(item);
+        }
+    }
+
+    private static void qu39() {
+        ArrayList<String> shoppingList = new ArrayList<>();
+        shoppingList.add("chicken");
+        shoppingList.add("pizza");
+        shoppingList.add("beef");
+        shoppingList.add("pasta");
+        shoppingList.add("tomato");
+        for (int i = 0; i < shoppingList.size(); i++) {
+            System.out.println(shoppingList.get(shoppingList.size() - i - 1));
+        }
+    }
+
+    private static void qu40(int[] numbers) {
+        for (int number : numbers) {
+            System.out.println(number);
+        }
+    }
+
+    private static void qu41(int[] numbers) {
+        int min = 0, max = 0, sum = 0;
+        for (int number : numbers) {
+            System.out.println(number);
+            if (number < min) {
+                min = number;
+            }
+            if (number > max) {
+                max = number;
+            }
+            sum += number;
+        }
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+        System.out.println("Sum: " + sum);
+    }
+
+    private static void qu42(int[] numbers) {
+        int min = numbers[0], max = numbers[0], sum = 0;
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < min) {
+                min = numbers[i];
+            }
+            else if (numbers[i] > max) {
+                max = numbers[i];
+            }
+            System.out.println(numbers[i]);
+            sum += numbers[i];
+        }
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+        System.out.println("Sum: " + sum);
+    }
+
+    private static void qu43(int dayOfWeek) {
+        switch (dayOfWeek) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("Wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("Sunday");
+                break;
+            default:
+                System.out.println("Invalid day.");
+                break;
+        }
+    }
+
+    private static void qu44(String month) {
+        switch (month) {
+            case "Jan":
+                System.out.println("01 - January (Jan)");
+                break;
+            case "Feb":
+                System.out.println("01 - February (Feb)");
+                break;
+            case "Mar":
+                System.out.println("01 - March (Mar)");
+                break;
+            case "Apr":
+                System.out.println("01 - April (Apr)");
+                break;
+            case "May":
+                System.out.println("01 - May (May)");
+                break;
+            case "Jun":
+                System.out.println("01 - June (Jun)");
+                break;
+            case "Jul":
+                System.out.println("01 - July (Jul)");
+                break;
+            case "Aug":
+                System.out.println("01 - August (Aug)");
+                break;
+            case "Sep":
+                System.out.println("01 - September (Sep)");
+                break;
+            case "Oct":
+                System.out.println("01 - October (Oct)");
+                break;
+            case "Nov":
+                System.out.println("01 - November (Nov)");
+                break;
+            case "Dec":
+                System.out.println("01 - December (Dec)");
+                break;
+            default:
+                System.out.println("-1 - Invalid month");
+                break;
+        }
+    }
+
+    private static void qu45(char grade) {
+        switch (grade) {
+            case 'S':
+                System.out.println(100);
+                break;
+            case 'A':
+                System.out.println(90);
+                break;
+            case 'B':
+                System.out.println(80);
+                break;
+            case 'C':
+                System.out.println(70);
+                break;
+            case 'D':
+                System.out.println(60);
+                break;
+            case 'E':
+                System.out.println(50);
+                break;
+            case 'F':
+                System.out.println(30);
+                break;
+            case 'U':
+                System.out.println(0);
+                break;
+
+        }
+    }
+
+    private static void qu46(int score) {
+        switch (score/10) {
+            case 10:
+                System.out.println('S');
+                break;
+            case 9:
+                System.out.println('A');
+                break;
+            case 8:
+                System.out.println('B');
+                break;
+            case 7:
+                System.out.println('C');
+                break;
+            case 6:
+                System.out.println('D');
+                break;
+            case 5:
+                System.out.println('E');
+                break;
+            case 4:
+            case 3:
+                System.out.println('F');
+                break;
+            default:
+                System.out.println('U');
+        }
     }
 }
